c8007569befcfc46729a2fa5c88d0a372a912081
dflozanot <daigo_1001@hotmail.com>
2017-06-09 10:20:06 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+<title>.::Periodismo::.</title>
+<meta charset="UTF-8">
+<meta name="viewport"
+	content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+<link rel="stylesheet" href="assets/css/estilos.css" />
+</head>
+<body>
+	<header>
+		<div id="header">
+			<div class="contenedor">
+				<h1>P.S</h1>
+				<input type="checkbox" id="menu-bar"> <label for="menu-bar"><img
+					src="assets/img/menu-bar.png" width="40px"></label>
+				<nav class="menu">
+					<a href="#">Inicio</a> <a href="investigacion.php">Investigaci&oacute;n</a> <a href="seguridad.php">Seguridad</a>
+					<a href="">...</a>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<section id="banner">
+		<img src="assets/img/2.jpg" />
+		<div class="contenedor">
+			<h2>Exposici&oacute;n</h2>
+			<p>Investigaci&oacute;n - Seguridad</p>
+			<a href="contacto.php">leer m&aacute;s</a>
+		</div>
+	</section>
+	<section id="bienvenidos">
+		<h2>Bienvenidos a la exposici&oacute;n</h2>
+		<p>La presente exposici&oacute;n a los temas:
+			"Investigaci&oacute;n y seguridad informatica"</p>
+	</section>
+	<section id="blog">
+		<h3>Imagenes relacionadas</h3>
+		<div class="contenedor">
+			<article>
+				<img src="assets/img/1.jpg">
+				<h4>Investigaci&oacute;n</h4>
+			</article>
+			<article>
+				<img src="assets/img/3.jpg">
+				<h4>Pensamiento Sistemico!</h4>
+			</article>
+			<article>
+				<img src="assets/img/5.jpg">
+				<h4>Seguridad Informatica</h4>
+			</article>
+		</div>
+	</section>
+
+	<section id="info">
+		<h3>Links</h3>
+		<div class="contenedor">
+			<div class="info-bd">
+				<a href="investigacion.php">
+				 <img alt="" src="assets/img/1.jpg">
+				</a>
+				<h4>Investigaci&oacute;n</h4>
+			</div>
+			<div class="info-bd">
+				<img alt="" src="assets/img/3.jpg">
+				<h4>Pensamiento Sistemico</h4>
+			</div>
+			<div class="info-bd">
+				<img alt="" src="assets/img/4.jpg">
+				<h4>Seguridad</h4>
+			</div>
+			<div class="info-bd">
+				<a href="seguridad.php"> 
+					<img alt="" src="assets/img/5.jpg">
+				</a>
+				<h4>Seguridad Informatica</h4>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="contenedor">
+			<p class="copy">Pensamiento Sistemico &copy; 2016</p>
+			<div class="sociales">
+				<a href=""><img src="assets/img/facebook.png" /></a> <a href=""><img
+					src="assets/img/google.png" /></a> <a href=""><img
+					src="assets/img/twitter.png" /></a> <a href=""><img
+					src="assets/img/youtube.png" /></a>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
